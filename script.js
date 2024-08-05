a6852ca410f1709759b82999e7688f6837773f54
aDm8H <132069645+aDm8H@users.noreply.github.com>
2024-08-05 12:15:47 +0800

    补充版权声明

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// <!-- Copyright © 2023 aDm8H -->
+// <!-- This file is part of b-fans-record-front.
+
+// b-fans-record-front is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+// b-fans-record-front is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License along with b-fans-record-front. If not, see <https://www.gnu.org/licenses/>. -->
 const $ = mdui.$;
 const isDark = moment().hour() >= 6 && moment().hour() < 18 ? false : true;
 if (isDark) $("body")[0].classList += "mdui-theme-layout-dark";
